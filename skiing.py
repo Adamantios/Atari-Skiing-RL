0e2f005963a32c65b9f598c2be44d218819b5a94
Adamantios <manoszaras94@gmail.com>
2019-04-30 17:00:13 +0300

    Add comments.

--- skiing.py
+++ skiing.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,9 +12,18 @@ from utils import create_path, atari_preprocess
 
 
 def create_skiing_environment():
+    """
+    Creates a skiing environment.
+
+    :return: the skiing environment, the initial state, the image's height and width and the action space's size.
+    """
+    # Create the skiing environment.
     environment = gym.make('Skiing-v0')
+    # Reset the environment and get the initial state.
     init_state = environment.reset()
+    # Get the observation space's height and width.
     height, width = environment.observation_space.shape[0], environment.observation_space.shape[1]
+    # Get the number of possible moves.
     act_space_size = environment.action_space.n
 
     return environment, init_state, height, width, act_space_size
@@ -24,12 +35,15 @@ def render_frame() -> None:
         env.render()
 
 
-def game_loop():
+def game_loop() -> None:
+    """ Starts the game loop and trains the agent. """
+    # Run for a number of episodes.
     for episode in range(nEpisodes):
+        # Init vars.
         global epsilon
         max_score, score, done = 0, 0, False
 
-        # Reset the environment.
+        # Reset and render the environment.
         current_state = env.reset()
         render_frame()
 
@@ -54,7 +68,7 @@ def game_loop():
             render_frame()
 
             next_state = atari_preprocess(next_state, downsample_scale)
-            next_state = np.append(next_state, current_state[:, :, :, :5], axis=3)
+            next_state = np.append(next_state, current_state[:, :, :, :], axis=3)
 
             replay_memory.append((current_state, action, reward, next_state))
 
@@ -77,6 +91,7 @@ def game_loop():
 
 
 if __name__ == '__main__':
+    # Create the default parameters.
     filename_prefix = 'out/atari_skiing'
     render = True
     downsample_scale = 2
@@ -91,17 +106,25 @@ if __name__ == '__main__':
     target_model_change = 100
     replay_memory_size = 400000
 
+    # Create the path to the file, if necessary.
     create_path(filename_prefix)
 
+    # Create the skiing environment.
     env, state, pixel_rows, pixel_columns, action_space_size = create_skiing_environment()
+    # Create the observation space's shape.
     observation_space_shape = (pixel_rows // downsample_scale, pixel_columns // downsample_scale, steps_per_action)
 
+    # Create the replay memory for the agent.
     replay_memory = deque(maxlen=replay_memory_size)
 
+    # Create the optimizer.
     optimizer = RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
+    # Create the model and the target model.
     model = atari_skiing_model(observation_space_shape, action_space_size, optimizer)
     target_model = atari_skiing_model(observation_space_shape, action_space_size, optimizer)
 
+    # Create the agent.
     agent = DQN(model, target_model, replay_memory, gamma, batch_size, observation_space_shape, action_space_size)
 
+    # Start the game loop.
     game_loop()
