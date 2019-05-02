495e1d12ab5d061b79d12252d86cdbae3e08d5d8
Adamantios <manoszaras94@gmail.com>
2019-05-02 20:38:04 +0300

    Tidy up code.

---
+++ core/game.py
@@ -0,0 +1,110 @@
+from collections import Generator
+import gym
+from gym.wrappers import TimeLimit
+from math import inf, ceil
+import numpy as np
+
+from core.agent import DQN
+from utils.preprocessing import atari_preprocess
+from utils.scoring import Scorer
+
+
+class Game(object):
+    def __init__(self, episodes: int, render: bool, downsample_scale: int, scorer: Scorer, agent_frame_history: int):
+        self._episodes = episodes
+        self._render = render
+        self._downsample_scale = downsample_scale
+        self._scorer = scorer
+        self._agent_frame_history = agent_frame_history
+
+        # Create the skiing environment.
+        self._env, self.pixel_rows, self.pixel_columns, self.action_space_size = self._create_skiing_environment()
+
+        # Create the observation space's shape.
+        self.observation_space_shape = (ceil(self.pixel_rows / self._downsample_scale),
+                                        ceil(self.pixel_columns / self._downsample_scale),
+                                        self._agent_frame_history)
+
+    @staticmethod
+    def _create_skiing_environment() -> [TimeLimit, int, int, int]:
+        """
+        Creates a skiing environment.
+
+        :return: the skiing environment, the image's height, the image's width and the action space's size.
+        """
+        # Create the skiing environment.
+        environment = gym.make('Skiing-v0')
+        # Get the observation space's height and width.
+        height, width = environment.observation_space.shape[0], environment.observation_space.shape[1]
+        # Get the number of possible moves.
+        act_space_size = environment.action_space.n
+
+        return environment, height, width, act_space_size
+
+    def _render_frame(self) -> None:
+        """ Renders a frame, only if the user has chosen to do so. """
+        if self._render:
+            self._env.render()
+
+    def play_game(self, agent: DQN) -> Generator:
+        """
+        Starts the game loop and trains the agent.
+
+        :param agent: the agent to play the game.
+        :return: generator containing the finished episode number.
+        """
+        # Run for a number of episodes.
+        for episode in range(1, self._episodes + 1):
+            # Init vars.
+            max_score, total_score, done = -inf, 0, False
+
+            # Reset and render the environment.
+            current_state = self._env.reset()
+            self._render_frame()
+
+            # Preprocess current_state.
+            current_state = atari_preprocess(current_state, self._downsample_scale)
+
+            # Create preceding frames, using the starting frame.
+            current_state = np.stack(tuple([current_state for _ in range(self._agent_frame_history)]), axis=2)
+
+            # Set current state with the stacked.
+            current_state = np.reshape(current_state,
+                                       (1,
+                                        ceil(self.pixel_rows / self._downsample_scale),
+                                        ceil(self.pixel_columns / self._downsample_scale),
+                                        self._agent_frame_history))
+
+            while not done:
+                # Take an action, using the policy.
+                action = agent.take_action(current_state)
+                # Take a step, using the action.
+                next_state, reward, done, _ = self._env.step(action)
+                # Render the frame.
+                self._render_frame()
+
+                # Preprocess the state.
+                next_state = atari_preprocess(next_state, self._downsample_scale)
+                # Append the frame history.
+                next_state = np.append(next_state, current_state[:, :, :, :self._agent_frame_history - 1], axis=3)
+
+                # Save sample <s,a,r,s'> to the replay memory.
+                agent.append_to_memory(current_state, action, reward, next_state)
+                # Fit agent and keep fitting history.
+                fitting_history = agent.fit()
+                if fitting_history is not None:
+                    self._scorer.huber_loss_history[episode - 1] += fitting_history.history['loss']
+
+                # Add reward to the total score.
+                total_score += reward
+                # Set current state with the next.
+                current_state = next_state
+                # Set max score.
+                max_score = max(max_score, reward)
+
+            # Add scores to the scores arrays.
+            self._scorer.max_scores[episode - 1] = max_score
+            self._scorer.total_scores[episode - 1] = total_score
+
+            # Yield the finished episode.
+            yield episode
--- skiing.py
+++ skiing.py
@@ -2,19 +2,18 @@ from os import path
 from typing import Union
 from warnings import warn
 
-import gym
 from collections import deque
-import numpy as np
+
 from keras.optimizers import adam, rmsprop, sgd, adagrad, adadelta, adamax
-from math import inf, ceil
 
 from core.agent import EGreedyPolicy, DQN
+from core.game import Game
 from core.model import atari_skiing_model, huber_loss, frame_can_pass_the_net, min_frame_dim_that_passes_net
 
 from utils.os_operations import create_path
 from utils.parser import create_parser
 from utils.plotting import Plotter
-from utils.preprocessing import atari_preprocess
+
 from utils.scoring import Scorer
 
 
@@ -49,10 +48,10 @@ def run_checks() -> None:
 
     # Downsampling should result with at least 32 pixels on each dimension,
     # because the first convolutional layer has a filter 8x8 with stride 4x4.
-    if not frame_can_pass_the_net(observation_space_shape[0], observation_space_shape[1]):
+    if not frame_can_pass_the_net(game.observation_space_shape[0], game.observation_space_shape[1]):
         raise ValueError('Downsample is too big. It can be set from 1 to {}'
-                         .format(min(int(pixel_rows / min_frame_dim_that_passes_net()),
-                                     int(pixel_columns / min_frame_dim_that_passes_net()))))
+                         .format(min(int(game.pixel_rows / min_frame_dim_that_passes_net()),
+                                     int(game.pixel_columns / min_frame_dim_that_passes_net()))))
 
     if plot_train_results and episodes == 1:
         warn('Cannot plot for 1 episode only.')
@@ -75,24 +74,6 @@ def run_checks() -> None:
     if total_observe_count < poor_observe:
         warn('The total number of observing steps ({}) is too small and could bring poor results.'
              'Consider a value grater than {}'.format(total_observe_count, poor_observe))
-
-
-def create_skiing_environment():
-    """
-    Creates a skiing environment.
-
-    :return: the skiing environment, the initial state, the image's height and width and the action space's size.
-    """
-    # Create the skiing environment.
-    environment = gym.make('Skiing-v0')
-    # Reset the environment and get the initial state.
-    init_state = environment.reset()
-    # Get the observation space's height and width.
-    height, width = environment.observation_space.shape[0], environment.observation_space.shape[1]
-    # Get the number of possible moves.
-    act_space_size = environment.action_space.n
-
-    return environment, init_state, height, width, act_space_size
 
 
 def initialize_optimizer() -> Union[adam, rmsprop, sgd, adagrad, adadelta, adamax]:
@@ -129,22 +110,17 @@ def create_agent() -> DQN:
         print('Agent {} has been loaded successfully.'.format(agent_path))
     else:
         # Init the model.
-        model = atari_skiing_model(observation_space_shape, action_space_size, optimizer)
+        model = atari_skiing_model(game.observation_space_shape, game.action_space_size, optimizer)
         # Create the replay memory for the agent.
         memory = deque(maxlen=replay_memory_size)
 
     # Create the policy.
-    policy = EGreedyPolicy(epsilon, final_epsilon, epsilon_decay, total_observe_count, action_space_size)
+    policy = EGreedyPolicy(epsilon, final_epsilon, epsilon_decay, total_observe_count, game.action_space_size)
     # Create the agent.
-    dqn = DQN(model, target_model_change, memory, gamma, batch_size, observation_space_shape, action_space_size, policy)
+    dqn = DQN(model, target_model_change, memory, gamma, batch_size, game.observation_space_shape,
+              game.action_space_size, policy)
 
     return dqn
-
-
-def render_frame() -> None:
-    """ Renders a frame, only if the user has chosen to do so. """
-    if render:
-        env.render()
 
 
 def save_agent(episode: int) -> None:
@@ -181,65 +157,6 @@ def end_of_episode_actions(episode: int) -> None:
 
     if results_save_interval > 0 and (episodes % results_save_interval == 0 or results_save_interval == 1):
         scorer.save_results(episode)
-
-
-def game_loop() -> None:
-    """ Starts the game loop and trains the agent. """
-    # Run for a number of episodes.
-    for episode in range(1, episodes + 1):
-        # Init vars.
-        max_score, total_score, done = -inf, 0, False
-
-        # Reset and render the environment.
-        current_state = env.reset()
-        render_frame()
-
-        # Preprocess current_state.
-        current_state = atari_preprocess(current_state, downsample_scale)
-
-        # Create preceding frames, using the starting frame.
-        current_state = np.stack(tuple([current_state for _ in range(agent_frame_history)]), axis=2)
-
-        # Set current state with the stacked.
-        current_state = np.reshape(current_state,
-                                   (1,
-                                    ceil(pixel_rows / downsample_scale),
-                                    ceil(pixel_columns / downsample_scale),
-                                    agent_frame_history))
-
-        while not done:
-            # Take an action, using the policy.
-            action = agent.take_action(current_state)
-            # Take a step, using the action.
-            next_state, reward, done, _ = env.step(action)
-            # Render the frame.
-            render_frame()
-
-            # Preprocess the state.
-            next_state = atari_preprocess(next_state, downsample_scale)
-            # Append the frame history.
-            next_state = np.append(next_state, current_state[:, :, :, :agent_frame_history - 1], axis=3)
-
-            # Save sample <s,a,r,s'> to the replay memory.
-            agent.append_to_memory(current_state, action, reward, next_state)
-            # Fit agent and keep fitting history.
-            fitting_history = agent.fit()
-            if fitting_history is not None:
-                scorer.huber_loss_history[episode - 1] += fitting_history.history['loss']
-
-            # Add reward to the total score.
-            total_score += reward
-            # Set current state with the next.
-            current_state = next_state
-            # Set max score.
-            max_score = max(max_score, reward)
-
-        # Add scores to the scores arrays.
-        scorer.max_scores[episode - 1] = max_score
-        scorer.total_scores[episode - 1] = total_score
-
-        # Take end of episode specific actions.
-        end_of_episode_actions(episode)
 
 
 if __name__ == '__main__':
@@ -279,13 +196,11 @@ if __name__ == '__main__':
     fuzz = args.fuzz
     momentum = args.momentum
 
-    # Create the skiing environment.
-    env, state, pixel_rows, pixel_columns, action_space_size = create_skiing_environment()
+    # Create a scorer.
+    scorer = Scorer(episodes, info_interval_mean, results_name_prefix)
 
-    # Create the observation space's shape.
-    observation_space_shape = (ceil(pixel_rows / downsample_scale),
-                               ceil(pixel_columns / downsample_scale),
-                               agent_frame_history)
+    # Create the game.
+    game = Game(episodes, render, downsample_scale, scorer, agent_frame_history)
 
     # Check arguments.
     run_checks()
@@ -296,11 +211,10 @@ if __name__ == '__main__':
     # Create the agent.
     agent = create_agent()
 
-    # Create a scorer.
-    scorer = Scorer(episodes, info_interval_mean, results_name_prefix)
-
     # Create a plotter.
     plotter = Plotter(episodes, plots_name_prefix, plot_train_results, save_plot)
 
     # Start the game loop.
-    game_loop()
+    for finished_episode in game.play_game(agent):
+        # Take specific actions after the end of each episode.
+        end_of_episode_actions(finished_episode)
