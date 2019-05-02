19b250e9f75fa970f0b4b98d4830809e9693d5fc
Adamantios <manoszaras94@gmail.com>
2019-05-02 14:33:05 +0300

    Check numbers to be greater than 0.

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentTypeError
 from os import makedirs, path
 import numpy as np
 
@@ -87,21 +87,50 @@ def create_parser() -> ArgumentParser:
     batch_size = 32
     gamma = .99
 
+    def positive_int(value: any) -> int:
+        """
+        Checks if a value is a positive integer.
+
+        :param value: the value to be checked.
+        :return: the value if valid integer, otherwise raises an ArgumentTypeError.
+        """
+        int_value = int(value)
+
+        if int_value <= 0:
+            raise ArgumentTypeError("%s should be a positive integer value." % value)
+
+        return int_value
+
+    def positive_float(value: any) -> float:
+        """
+        Checks if a value is a positive float.
+
+        :param value: the value to be checked.
+        :return: the value if valid float, otherwise raises an ArgumentTypeError.
+        """
+        float_value = float(value)
+
+        if float_value <= 0:
+            raise ArgumentTypeError("%s should be a positive float value." % value)
+
+        return float_value
+
     parser = ArgumentParser(description='Trains a DQN agent to play the Atari Skiing game.')
 
     parser.add_argument('-f', '--filename', type=str, required=False, default=filename_prefix,
                         help='Filename prefix for the trained model to be saved (default %(default)s).')
-    parser.add_argument('-si', '--save_interval', type=int, default=save_interval, required=False,
+    parser.add_argument('-si', '--save_interval', type=positive_int, default=save_interval, required=False,
                         help='The save interval for the trained model (default %(default)s), in episodes.')
-    parser.add_argument('-iic', '--info_interval_current', type=int, default=info_interval_current, required=False,
+    parser.add_argument('-iic', '--info_interval_current',
+                        type=positive_int, default=info_interval_current, required=False,
                         help='The current scoring information interval (default %(default)s), in episodes.')
-    parser.add_argument('-iim', '--info_interval_mean', type=int, default=info_interval_mean, required=False,
+    parser.add_argument('-iim', '--info_interval_mean', type=positive_int, default=info_interval_mean, required=False,
                         help='The mean scoring information interval (default %(default)s), in episodes.')
-    parser.add_argument('-ti', '--target_interval', type=int, default=target_model_change, required=False,
+    parser.add_argument('-ti', '--target_interval', type=positive_int, default=target_model_change, required=False,
                         help='The target model change interval (default %(default)s), in steps.')
     parser.add_argument('-a', '--agent', type=str, required=False, default=agent_path,
                         help='Filepath for a trained agent to be loaded (default %(default)s).')
-    parser.add_argument('-ah', '--agent_history', type=int, required=False, default=agent_history,
+    parser.add_argument('-ah', '--agent_history', type=positive_int, required=False, default=agent_history,
                         help='The agent\'s frame history (default %(default)s).')
     parser.add_argument('-np', '--no_plot', default=not plot_train_results, required=False, action='store_false',
                         help='Whether the train results should not be plot (default %(default)s).')
@@ -111,27 +140,27 @@ def create_parser() -> ArgumentParser:
                         help='Filename for the plot to be saved (default %(default)s).')
     parser.add_argument('-nr', '--no_render', default=not render, required=False, action='store_false',
                         help='Whether the environment should not be rendered (default %(default)s).')
-    parser.add_argument('-d', '--downsample', type=int, default=downsample_scale, required=False,
+    parser.add_argument('-d', '--downsample', type=positive_int, default=downsample_scale, required=False,
                         help='The downsampling scale to be used (default %(default)s).')
-    parser.add_argument('-s', '--steps', type=int, default=steps_per_action, required=False,
+    parser.add_argument('-s', '--steps', type=positive_int, default=steps_per_action, required=False,
                         help='The steps to skip per action (default %(default)s).')
-    parser.add_argument('-e', '--episodes', type=int, default=episodes, required=False,
+    parser.add_argument('-e', '--episodes', type=positive_int, default=episodes, required=False,
                         help='The episodes to run the training procedure (default %(default)s).')
-    parser.add_argument('-eps', '--epsilon', type=int, default=epsilon, required=False,
+    parser.add_argument('-eps', '--epsilon', type=positive_float, default=epsilon, required=False,
                         help='The epsilon for the e-greedy policy (default %(default)s).')
-    parser.add_argument('-feps', '--final_epsilon', type=int, default=final_epsilon, required=False,
+    parser.add_argument('-feps', '--final_epsilon', type=positive_float, default=final_epsilon, required=False,
                         help='The final epsilon for the e-greedy policy (default %(default)s).')
-    parser.add_argument('-deps', '--decay', type=int, default=epsilon_decay, required=False,
+    parser.add_argument('-deps', '--decay', type=positive_float, default=epsilon_decay, required=False,
                         help='The epsilon decay for the e-greedy policy (default %(default)s).')
-    parser.add_argument('-o', '--observe', type=int, default=total_observe_count, required=False,
+    parser.add_argument('-o', '--observe', type=positive_int, default=total_observe_count, required=False,
                         help='The total number of observing steps before the training begins, '
                              'thus taking random actions (default %(default)s).')
-    parser.add_argument('-rm', '--replay_memory', type=int, default=replay_memory_size, required=False,
+    parser.add_argument('-rm', '--replay_memory', type=positive_int, default=replay_memory_size, required=False,
                         help='The replay memory to be used for the agent (default %(default)s).')
-    parser.add_argument('-b', '--batch', type=int, default=batch_size, required=False,
+    parser.add_argument('-b', '--batch', type=positive_int, default=batch_size, required=False,
                         help='The batch size to be randomly sampled from the memory for the training '
                              '(default %(default)s).')
-    parser.add_argument('-g', '--gamma', type=int, default=gamma, required=False,
+    parser.add_argument('-g', '--gamma', type=positive_float, default=gamma, required=False,
                         help='The discount factor (default %(default)s).')
 
     return parser
